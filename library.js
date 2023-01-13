4904ac385b9ad8fe88a14fed63983fb432394c39
neelg07 <ineel7762@gmail.com>
2023-01-12 23:18:26 -0500

    Add basic code structure for beginning of proj.

---
+++ library.js
@@ -0,0 +1,19 @@
+// Library App
+let myLibrary = ['Harry Potter', 'Apple Tree', 'Sample'];
+
+function Book(title, author, pages, read) {
+    // constructor
+}
+
+function addBookToLibrary() {
+    //
+}
+
+function displayLibrary() {
+    // Iterate array of books
+    for (let book of myLibrary) {
+        console.log(book);
+    }
+}
+
+displayLibrary();
