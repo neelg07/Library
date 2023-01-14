f89c462a6b9e6d40128108eca9ffc28476be50b8
neelg07 <ineel7762@gmail.com>
2023-01-14 17:33:49 -0500

    Add constructor func to Book class; Add function to addBookToLibrary i.e. book obj to myLibrary array; Add formSubmit eventListener with function to call constructor and addBookToLibrary;

--- library.js
+++ library.js
@@ -3,10 +3,15 @@ let myLibrary = ['Harry Potter', 'Apple Tree', 'Sample'];
 
 function Book(title, author, pages, read) {
     // constructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(obj) {
     //
+    myLibrary.push(obj);
 }
 
 function displayLibrary() {
@@ -23,9 +28,9 @@ const addBook = document.querySelector('.add-icon');
 
 const bookForm = document.querySelector('.bookform');
 
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const pages = document.getElementById('pages');
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
 const readRadio = document.getElementById('read');
 const unreadRadio = document.getElementById('unread');
 
@@ -35,13 +40,13 @@ addBook.addEventListener('click', () => {
     bookForm.classList.toggle('inactive');
 
     // Reset input fields and set focus on title input
-    title.value = '';
-    author.value = '';
-    pages.value = '';
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
     readRadio.checked = false;
     unreadRadio.checked = false;
 
-    title.focus();
+    titleInput.focus();
 });
 
 // Close form button
@@ -50,4 +55,20 @@ const closeForm = document.getElementById('close-btn');
 closeForm.addEventListener('click', () => {
     //Close out form
     bookForm.classList.toggle('inactive');
+});
+
+// Submit form --> Create new book object with form data
+formSubmit = document.querySelector('.submit-form');
+
+formSubmit.addEventListener('submit', (event) => {
+    // Save input values to variables
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let pages = pagesInput.value;
+    let read; 
+    readRadio.checked ? read = true : read = false;
+    
+    // Create book obj
+    let newBook = new Book(title, author, pages, read); 
+    addBookToLibrary(newBook);
 });
