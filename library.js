db700a5904ad95e49a56bc362da3774bbc98aa8a
neelg07 <ineel7762@gmail.com>
2023-01-14 16:14:12 -0500

    Add toggle classes to display/hide form; Add reset to form inputs when closing/reopening unfinished form

--- library.js
+++ library.js
@@ -21,6 +21,25 @@ displayLibrary();
 // Add book button
 const addBook = document.querySelector('.add-icon');
 
+const bookForm = document.querySelector('.bookform');
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const readRadio = document.getElementById('read');
+const unreadRadio = document.getElementById('unread');
+
+// Add Book button
 addBook.addEventListener('click', () => {
-    // Popup form
+    // Popup form toggle
+    bookForm.classList.toggle('inactive');
+
+    // Reset input fields and set focus on title input
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    readRadio.checked = false;
+    unreadRadio.checked = false;
+
+    title.focus();
 });
